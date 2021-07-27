b05144cbad1c3f73638f77441f956009074d25f2
Santiago Rosas <87668523+RosasSantiago@users.noreply.github.com>
2021-07-27 00:19:47 -0300

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,5 +1,4 @@
 # Selenium Imports
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
